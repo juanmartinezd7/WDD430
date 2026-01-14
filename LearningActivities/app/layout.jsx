a6d18cf3697b4f11aba5dd3fc7da56b9162ef39e
juanmartinezd7 <rysmartinez7@gmail.com>
2026-01-13 19:49:35 -0600

    Initial commit (clean, no node_modules)

---
+++ LearningActivities/app/layout.jsx
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: "WDD 430",
+  description: "Learning Activities",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
